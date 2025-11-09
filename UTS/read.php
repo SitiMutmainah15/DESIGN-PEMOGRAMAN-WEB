b9de2b9a01b0e50c013f27bd28133b4de4f9696b
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2025-11-09 11:00:16 +0700

    Delete UTS/read.php

--- UTS/read.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-//header("Content-Type: application/json");
-
-$result = pg_query($conn, "SELECT * FROM menu ORDER BY id ASC");
-$data = pg_fetch_all($result);
-
-echo json_encode($data ?: []);
-?>
